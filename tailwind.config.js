0686961f4ae12141ef6bc28e2aaffcf581fe7481
Luca Casonato <lucacasonato@yahoo.com>
2020-07-05 14:42:17 +0200

    New site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   purge: ["./components/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   plugins: [require("@tailwindcss/ui")],
 };
