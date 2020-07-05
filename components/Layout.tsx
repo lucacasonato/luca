e5feda154b5786afa009b9e48b991f9c34c47957
Luca Casonato <lucacasonato@yahoo.com>
2020-07-05 14:45:41 +0200

    Update tailwind config

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,7 +8,6 @@ function Layout(props: { title: string; description: string; children: any }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{props.title}</title>
         <meta name="Description" content={props.description} />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         <link rel="stylesheet" href="/app.css" />
       </head>
       <body>{props.children}</body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./components/**/*.tsx"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
   plugins: [require("@tailwindcss/ui")],
 };
