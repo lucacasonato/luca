cbddedea15d31a5bb74f26df1637c80ce747a892
Luca Casonato <hello@lcas.dev>
2021-08-07 14:10:40 +0200

    import less stuff

--- deps.ts
+++ deps.ts
@@ -9,14 +9,5 @@ if (IS_BROWSER) {
   setup({});
 }
 
-// x/ammonia & x/comrak
-import * as ammonia from "https://deno.land/x/ammonia@0.3.0/mod.ts";
-import * as comrak from "https://deno.land/x/comrak@0.1.0/mod.ts";
-// await Promise.all([ammonia.init(), comrak.init()]);
-export { ammonia, comrak };
-
 // std
 export { join } from "https://deno.land/std@0.103.0/path/mod.ts";
-
-// frontmatter
-export { parse as frontmatterParse } from "https://deno.land/x/frontmatter@v0.1.2/mod.ts";
