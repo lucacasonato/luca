3329b4fdba9a49df258d8e4ed4582fdcfff97cfc
Luca Casonato <hello@lcas.dev>
2023-11-14 15:47:51 +0100

    build step

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@
 /// <reference lib="dom.iterable" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
+/// <reference lib="deno.unstable" />
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
@@ -10,6 +11,11 @@ import manifest from "./fresh.gen.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.js";
 
-throw new Error("This file should not be imported");
+const kv = await Deno.openKv();
+const build = await kv.get(["builds", Deno.env.get("DENO_DEPLOYMENT_ID")]);
+if (build.value === null) {
+  console.log("Do a build step here!");
+  await kv.set(["builds", Deno.env.get("DENO_DEPLOYMENT_ID")], "built");
+}
 
 await start(manifest, { plugins: [twindPlugin(twindConfig)] });
