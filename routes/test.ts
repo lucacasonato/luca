bcc764db48aefa9328f50b7eac6146adea2a658e
Luca Casonato <hello@lcas.dev>
2024-03-12 14:43:14 +0100

    fix

--- routes/test.ts
+++ routes/test.ts
@@ -2,5 +2,6 @@ import { Handler } from "$fresh/server.ts";
 
 export const handler: Handler = (req) => {
   console.log(req.headers);
+  console.log(req.headers.get("user-agent"));
   return new Response("Hello, world!");
 };
