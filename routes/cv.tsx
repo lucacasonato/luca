da92e31c3512bf9f1318c325aa63334fb1c63d78
Luca Casonato <hello@lcas.dev>
2026-03-06 00:34:13 +0100

    update cv

--- routes/cv.tsx
+++ routes/cv.tsx
@@ -17,7 +17,7 @@ const CV: CVItem[] = [
     title: "Lead Software Engineer",
     company: "Deno Land Inc",
     description:
-      "Technical lead for Deno Deploy product. Development on JSR (JavaScript Registry), and member of the JSR board. Engineering on the open source Deno project. Built the Fresh web framework. Work in web standards bodies as chair of TC55 (WinterTC) at ECMA, delegate of TC39 (committee that designs the JavaScript language), and contributions to WHATWG and W3C specs.",
+      "Technical lead for the Deno Deploy product. Development on JSR (JavaScript Registry), and member of the JSR board. Engineering on the open source Deno project. Built the Fresh web framework (v1). Work in web standards bodies as chair of TC55 (WinterTC) at ECMA, delegate at TC39 (committee that designs the JavaScript language), and contributions to WHATWG and W3C specs.",
   },
   {
     start: new Date(2019, 5),
@@ -66,40 +66,54 @@ export default define.page(function CVPage() {
           </h2>
           <ul class="mt-6 text-lg space-y-4">
             <li>
-              Highly proficient in <b>Rust</b>, <b>JavaScript</b>,{" "}
-              <b>TypeScript</b>, <b>Go</b>, <b>HTML</b>, and{" "}
-              <b>CSS</b>. Experience with <b>Dart</b>, <b>Java</b>, and{" "}
-              <b>C++</b>.
+              I am highly proficient in
+              <b>TypeScript</b>, <b>JavaScript</b>, <b>Rust</b>,{" "}
+              <b>HTML</b>, and <b>CSS</b>. I have experience with <b>Go</b>,
+              {" "}
+              <b>Dart</b>, <b>C++</b>.
             </li>
             <li>
-              Experienced in <b>web development</b>, <b>systems programming</b>,
-              {" "}
-              <b>networking</b>, and{" "}
-              <b>cloud computing</b>. Significant experience with <b>React</b>,
+              I have significant experience in <b>product engineering</b>,{" "}
+              <b>software architecture engineering</b>,{" "}
+              <b>(distributed) systems engineering</b>, and{" "}
+              <b>web development</b>. Significant knowledge of <b>React</b>,
               {" "}
               <b>Preact</b>, <b>TailwindCSS</b>, <b>Deno</b>, <b>Node.js</b>,
               {" "}
-              <b>PostgreSQL</b>, <b>Google Cloud Platform</b>,{" "}
-              <b>Open Telemetry</b>, and <b>Grafana</b>.
+              <b>PostgreSQL</b>, <b>GCP</b>, <b>AWS</b>, <b>Vultr</b> ,{" "}
+              <b>Kubernetes</b>, <b>Open Telemetry</b>, <b>Clickhouse</b>,{" "}
+              <b>Grafana</b>, and <b>Terraform</b>. Some experience with{" "}
+              <b>Talos</b>, <b>Firecracker</b>, <b>WebAssembly</b>.
             </li>
             <li>
-              Intimately familiar with the <b>web platform</b> and{" "}
-              <b>JavaScript runtimes</b>. I have implemented much of the APIs
-              specified in various WHATWG specifications, and am a delegate at
-              TC39.
+              I am intimately familiar with the <b>web platform</b> and{" "}
+              <b>JavaScript runtimes</b>. I have implemented many APIs specified
+              in various WHATWG specifications in Deno. I and am a delegate at
+              TC39, where I have (successfully) proposed changes to the
+              JavaScript language. I chair TC55 (WinterTC) at ECMA, which works
+              to specify common APIs for use across server-side JavaScript
+              runtimes.
             </li>
             <li>
-              Capable of architecting and implementing <b>scalable</b> and{" "}
-              <b>performant</b> web services.
+              I am a strong communicator and have experience in public speaking
+              (at conferences, on podcasts, and in product presentations),
+              writing technical documentation, and writing technical and product
+              blog posts. I very much enjoy talking to customers.
             </li>
             <li>
-              Significant experience in managing and maintaining{" "}
-              <b>large open source projects</b>. I am a core team member of the
+              I am very detail oriented and have the ability to convince others
+              of my vision. I have experience in proposing, designing,
+              implementing, testing, and overseeing the implementation of, large
+              features involving multiple engineers and product stakeholders.
+            </li>
+            <li>
+              I have significant experience in managing and maintaining{" "}
+              <b>large open source projects</b>. I was a core team member of the
               {" "}
               <b>Deno project</b>{" "}
-              which has over 1000 contributors and over 13200 commits. We work
-              through an average of 220 pull requests and 150 new issues a
-              month.
+              for many years, which has over 1000 separate contributors and over
+              15000 commits. The project works through an average of 300 pull
+              requests and 150 new issues a month.
             </li>
           </ul>
         </div>
@@ -110,12 +124,22 @@ export default define.page(function CVPage() {
           <ul class="mt-6 text-lg space-y-2">
             <li>
               <a
+                href="https://deno.com/deploy"
+                class="font-semibold hover:underline"
+              >
+                Deno Deploy
+              </a>{" "}
+              - the simplest platform to deploy your JavaScript and TypeScript
+              applications.
+            </li>
+            <li>
+              <a
                 href="https://github.com/denoland/deno"
                 class="font-semibold hover:underline"
               >
                 Deno
               </a>{" "}
-              - a modern runtime for JavaScript and TypeScript.
+              - a modern server-side runtime for JavaScript and TypeScript.
             </li>
             <li>
               <a
@@ -133,8 +157,8 @@ export default define.page(function CVPage() {
               >
                 Fresh
               </a>{" "}
-              - a web framework for building very fast websites with great
-              developer experience.
+              - a web framework for building very fast websites using Preact,
+              with great developer experience.
             </li>
           </ul>
         </div>
