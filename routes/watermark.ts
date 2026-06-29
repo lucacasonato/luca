f6d913f6e684d8b5490f29c8b236b66422c82211
Luca Casonato <hello@lcas.dev>
2026-06-29 12:24:42 +0200

    More detailed link

--- routes/watermark.ts
+++ routes/watermark.ts
@@ -3,7 +3,7 @@ import { define } from "@/utils.ts";
 export const handler = define.handlers({
   GET() {
     return Response.redirect(
-      "https://www.autoriteitpersoonsgegevens.nl/en/themes/identification/passport-and-identity-card/copy-of-your-id-what-can-you-do",
+      "https://www.autoriteitpersoonsgegevens.nl/en/themes/identification/passport-and-identity-card/copy-of-your-id-what-can-you-do#watermark-or-writing-on-copy-id",
     );
   },
 });
